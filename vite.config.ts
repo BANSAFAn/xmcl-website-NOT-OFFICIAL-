bd673b6833741124cce48bf70e308019bb9c8c1d
BANER <86590991+BANSAFAn@users.noreply.github.com>
2025-09-18 12:38:28 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tailwind from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,6 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    tailwind(),
   ].filter(Boolean),
   resolve: {
     alias: {
