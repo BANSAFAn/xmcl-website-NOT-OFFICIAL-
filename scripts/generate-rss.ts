c782b43e09536fc9d1b8c19d400602e11a3b7a9b
BANER <86590991+BANSAFAn@users.noreply.github.com>
2025-09-30 22:27:05 +0300

    Update generate-rss.ts

--- scripts/generate-rss.ts
+++ scripts/generate-rss.ts
@@ -5,7 +5,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import { getAllBlogPosts } from '../src/utils/blogUtils.ts';
 import { getAllGuidePosts } from '../src/utils/guideUtils.ts';
-import { generateRSSFeed, generateGuideRSSFeed } from '../src/utils/rssGenerator.ts';
+
 
 const siteUrl = 'https://xmcl-website-not-official.vercel.app';
 
